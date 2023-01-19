10add7487504c2c9aad172f269620d25675d2e2e
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-01-19 19:06:31 +0000

    Dashboard avec de nouvelles proprietes ajoutees au bd json

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,228 +1,49 @@
 //var dbSource="bd.json";
-​
-var dataBD = {
-    "categories": [
-        {
-            "id": 1,
-            "libelle": "Nounou"
-        },
-        {
-            "id": 2,
-            "libelle": "Chauffeur"
-        },
-        {
-            "id": 3,
-            "libelle": "Electricien"
-        },
-        {
-            "id": 4,
-            "libelle": "Coursier"
-        },
-        {
-            "id": 5,
-            "libelle": "Plombier"
-        },
-        {
-            "id": 6,
-            "libelle": "Domestique"
-        },
-        {
-            "id": 7,
-            "libelle": "Jardinier"
-        }
-    ],
-    "professionnels": [
-        {
-            "nom": "Kacou",
-            "prenom": "Jean-Phillipe",
-            "categorie_id": 1,
-            "email": "kacouphilippe@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            "notation": 4.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Mangou",
-            "prenom": "Olivier",
-            "categorie_id": 5,
-            "email": "mangouolivier@gmail.com",
-            "tel1": "+225 5486952314",
-            "tel2": "+225 4258796354",
-            "annee_experience": 2,
-            "notation": 3,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Agou",
-            "prenom": "Colette",
-            "categorie_id": 3,
-            "email": "colette_agou@gmail.com",
-            "tel1": "+225 8596741236",
-            "tel2": "+225 4525698732",
-            "annee_experience": 2,
-            "notation": 3.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Kata",
-            "prenom": "N'Douan Justine",
-            "categorie_id": 1,
-            "email": "kata_justine@yahoo.com",
-            "tel1": "+225 2352416985",
-            "tel2": "+225 2147856932",
-            "annee_experience": 6,
-            "notation": 3,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Sagou",
-            "prenom": "José Eliakim",
-            "categorie_id": 2,
-            "email": "sagoujose@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 6,
-            "notation": 1,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Akossi",
-            "prenom": "Benjo",
-            "categorie_id": 1,
-            "email": "akossi@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 7,
-            "notation": 3.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Non Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Danho",
-            "prenom": "Deborah",
-            "categorie_id": 1,
-            "email": "danho@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 7,
-            "notation": 3.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Boly",
-            "prenom": "Basile",
-            "categorie_id": 6,
-            "email": "basileboly@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 5,
-            "notation": 4,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Gnaly",
-            "prenom": "Ornella",
-            "categorie_id": 1,
-            "email": "gnaly@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            "notation": 4.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Non Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Kacou",
-            "prenom": "Jean-Phillipe",
-            "categorie_id": 1,
-            "email": "kacouphilippe@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            "notation": 4.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Kacou",
-            "prenom": "Jean-Phillipe",
-            "categorie_id": 1,
-            "email": "kacouphilippe@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            "notation": 4.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Kacou",
-            "prenom": "Jean-Phillipe",
-            "categorie_id": 1,
-            "email": "kacouphilippe@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            "notation": 4.5,
-            "fonction": "Expert menuisier",
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Kacou",
-            "prenom": "Jean-Phillipe",
-            "categorie_id": 1,
-            "email": "kacouphilippe@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            "notation": 4.5,
-            "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptates consectetur, alias animi facere rem ipsa numquam ipsam deleniti ducimus magni omnis veniam optio. Autem nostrum maxime sint ipsam quam.",
-            "statut":"Disponible",
-            "situation_geo":"Abidjan, Port-Bouet"
-        },
-        {
-            "nom": "Kacou",
-            "prenom": "Jean-Phillipe",
-            "categorie_id": 1,
-            "email": "kacouphilippe@gmail.com",
-            "tel1": "+225 0785964220",
-            "tel2": "+225 0145239874",
-            "annee_experience": 3,
-            &quo...}
+let professionnelconnecte;
+let profes = 1;//reccuperation du premier professionnel depuis la page loadbdjs
+
+for (const professionnel of professionnels) {
+  if (professionnel.id ==profes) {
+    professionnelconnecte = professionnel;
+    break;
+  }
+}
+
+let categoriePro;
+
+for (const categorie of categories) {
+  if ( categorie.id == professionnelconnecte.categorie_id) {
+       categoriePro = categorie.libelle;
+    break;
+  }
+}
+
+console.log(categories)
+
+let name = document.getElementById("name").value= professionnelconnecte.nom + " " + professionnelconnecte.prenom;
+
+let email = document.getElementById("email").value= professionnelconnecte.email 
+
+let tel1 = document.getElementById("tel1").value= professionnelconnecte.tel1
+
+let tel2 = document.getElementById("tel2").value= professionnelconnecte.tel2;
+
+let categoriepro = document.getElementById("categorie").value= categoriePro;
+
+let experience = document.getElementById("experience").value = professionnelconnecte.annee_experience;
+
+let casier_judiciaire = document.getElementById("casier-judiciaire").value=professionnelconnecte.casier_judiciaire;
+
+let piece = document.getElementById("piece").value = professionnelconnecte.piece_identite;
+
+let situation = document.getElementById("situation-geographique").value = professionnelconnecte.situation_geo;
+
+let login = document.getElementById("login").value =professionnelconnecte.login;
+
+let password = document.getElementById("password").value = professionnelconnecte.mot_de_passe;
+
+let biographie = document.getElementById("biographie").value = professionnelconnecte.description;
 
 
 
-  
+
